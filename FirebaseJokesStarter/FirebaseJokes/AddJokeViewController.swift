3a7ea70f8548c5ea8470603f08d27f124afb1963
zoufishanmehdi <zzmehdi@gmail.com>
2016-05-11 22:34:36 -0400

    add joke

--- FirebaseJokesStarter/FirebaseJokes/AddJokeViewController.swift
+++ FirebaseJokesStarter/FirebaseJokes/AddJokeViewController.swift
@@ -7,13 +7,25 @@
 //
 
 import UIKit
+import Firebase
 
 class AddJokeViewController: UIViewController {
     
     @IBOutlet weak var jokeField: UITextField!
+    var currentUsername = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        DataService.dataService.CURRENT_USER_REF.observeEventType(FEventType.Value, withBlock: { snapshot in
+            
+            let currentUser = snapshot.value.objectForKey("username") as! String
+            
+            print("Username: \(currentUser)")
+            self.currentUsername = currentUser
+            }, withCancelBlock: { error in
+                print(error.description)
+        })
     }
     
     override func didReceiveMemoryWarning() {
